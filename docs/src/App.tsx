4cb819551d1da097bc72ba884d3b766d783c3e0c
prnth <pranith_h@yahoo.com.sg>
2025-12-29 12:53:49 +0530

    build error

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import { createPublicClient, createWalletClient, http, defineChain, parseEther, formatEther, custom } from 'viem'
 import './App.css'
 

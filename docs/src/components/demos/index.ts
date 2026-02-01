10b769359e67c8f319bc081ca4d14a7a4dea7742
PockitCEO <prnthdotcom@gmail.com>
2026-02-01 05:57:29 +0530

    Add demo export index

---
+++ docs/src/components/demos/index.ts
@@ -0,0 +1,4 @@
+export { default as CoinFlip } from './CoinFlip'
+export { default as SimpleEscrow } from './SimpleEscrow'
+// export { default as RockPaperScissors } from './RockPaperScissors'
+// export { default as DrawAndJudge } from './DrawAndJudge'
